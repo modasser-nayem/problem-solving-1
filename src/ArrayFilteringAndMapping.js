9adf50ab85c307aeb9458e297585f85064bc72a5
modasser-nayem <modassernayem@gmail.com>
2024-07-11 23:33:17 +0600

    Solve Problem-1, Array Filtering And Mapping

---
+++ src/ArrayFilteringAndMapping.js
@@ -0,0 +1,49 @@
+// 1.Task: Array Filtering And Mapping
+
+// Create An Array Of Objects, Each Representing A Person With Properties Like Name, Age, And Gender. Write A Function To Filter Out All Females And Then Map The Remaining People To An Array Of Names. Print The Final Result.
+
+const persons = [
+   {
+      name: "Nayem",
+      age: 21,
+      gender: "male",
+   },
+   {
+      name: "Sayem",
+      age: 22,
+      gender: "male",
+   },
+   {
+      name: "Ruma",
+      age: 17,
+      gender: "female",
+   },
+   {
+      name: "Rahib",
+      age: 18,
+      gender: "male",
+   },
+   {
+      name: "Sopna",
+      age: 21,
+      gender: "female",
+   },
+];
+
+function remainingPeopleNames(persons) {
+   if (!Array.isArray(persons)) {
+      console.error("Only array accepted");
+      return;
+   } else {
+      const remainPeople = persons.filter(
+         (person) => person.gender.toLowerCase() !== "female"
+      );
+
+      const peopleNames = remainPeople.map((people) => people.name);
+
+      return peopleNames;
+   }
+}
+
+const result = remainingPeopleNames(persons);
+console.log("Result", result);
---
+++ src/ObjectManipulation.js
@@ -0,0 +1,3 @@
+// 2.Task: Object Manipulation
+
+// Create An Array Of Objects Representing Books With Properties Like Title, Author, And Year. Write A Function That Takes The Array And Returns A New Array With Only The Book Titles. Print The Result.
